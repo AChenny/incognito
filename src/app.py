0830783453b1b5cf55369467a2839bef2873937a
Anderson Chen <anderson_chen97@hotmail.com>
2020-06-04 00:58:32 -0700

    Added upload handler
    -Upload handler will be the API to handle uploading pdfs (Future for all
    files)

--- src/app.py
+++ src/app.py
@@ -10,7 +10,6 @@ BUCKET_NAME = 'incognito-file-storage-bucket' # Change this to your bucket name
 TEST_RESUME_FILENAME = 'Resume.pdf'
 
 def lambda_handler(event, context):
-    
     # Extract text from pdf
     jobId = textProcessHelper.startJob(BUCKET_NAME, TEST_RESUME_FILENAME)
     print("Started job with id: {}".format(jobId))
@@ -18,6 +17,16 @@ def lambda_handler(event, context):
     if (textProcessHelper.isJobComplete(jobId)):
         response = textProcessHelper.getJobResults(jobId)
 
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Successful request."
+        }),       
+    }
+
+def upload_handler(event, context):
+
+    
     return {
         "statusCode": 200,
         "body": json.dumps({
