096259f1b9465117a226e0d3c003720df1ff5d50
Anderson Chen <anderson_chen97@hotmail.com>
2020-06-06 14:09:09 -0700

    Added ptvsd to project for SAM Debugging
    -ptvsd is debugger package for use wqith visual studio code which is
    needed step debugging

--- src/app.py
+++ src/app.py
@@ -1,3 +1,9 @@
+import ptvsd
+
+# Enable ptvsd on 0.0.0.0 address and on port 5890 that we'll connect later with our IDE
+ptvsd.enable_attach(address=('0.0.0.0', 5890), redirect_output=True)
+ptvsd.wait_for_attach()
+
 # External libraries
 import json
 
@@ -25,8 +31,6 @@ def lambda_handler(event, context):
     }
 
 def upload_handler(event, context):
-
-    
     return {
         "statusCode": 200,
         "body": json.dumps({
