14cb2fb3ce0a72e55ef38b0f47e54b0374a1363c
Anderson Chen <anderson_chen97@hotmail.com>
2020-06-25 00:14:15 -0700

    Get bounding boxes function
    -Created a function to get bounding boxes for a list of filters from a
    block object textract response
    
    -Created a function in imageHelper for getting a PIL object from a pdf
    byte string
    	This will be useful for later when we want to edit the image so
    	we don't have to reconvert a image into a PIL object to edit it
    
    -Created a function to convert the object into a image byte string
    	To be used later

--- src/imageHelper.py
+++ src/imageHelper.py
@@ -6,18 +6,18 @@ import base64
 
 # TODO: Update this to handle more than 1 image
 # Description: Converts a pdf to image byte string
-# Input: PDF byte string
+# Input: PIL Image Object
 # Output: Image byte string
-def convert_pdf_to_image(pdfDocument):
-    images = pdf2image.convert_from_bytes(pdfDocument, dpi=200, fmt='png')
+def convert_object_to_image_string(imageObject):
+    imgArr = io.BytesIO()
+    img.save(imgArr, format='png')
     
-    return_dict = {'images': []}
-    for img in images:
-        imgArr = io.BytesIO()
-        img.save(imgArr, format='png')
+    return imgArr.getvalue()
 
-        return_dict['images'].append(
-            imgArr.getvalue()
-        )
-    
-    return return_dict['images'][0]
+# TODO: Update this to handle more than 1 image
+# Description: Convert a pdf image to PIL object
+# Input: PDF byte string
+# Output PIL object
+def get_image_object_from_pdf(pdfDocument):
+    images = pdf2image.convert_from_bytes(pdfDocument, dpi=200, fmt='png')
+    return images[0]
